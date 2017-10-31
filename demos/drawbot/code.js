261b6b9e10b64146787b5eb88a4c86c96f7376b3
Felix Schulthess <fsch@truthbox.ch>
2017-10-31 23:28:44 +0100

    Set default language to German.

--- demos/drawbot/code.js
+++ demos/drawbot/code.js
@@ -108,8 +108,8 @@ Code.getStringParamFromUrl = function(name, defaultValue) {
 Code.getLang = function() {
   var lang = Code.getStringParamFromUrl('lang', '');
   if (Code.LANGUAGE_NAME[lang] === undefined) {
-    // Default to English.
-    lang = 'en';
+    // Default to German.
+    lang = 'de';
   }
   return lang;
 };
